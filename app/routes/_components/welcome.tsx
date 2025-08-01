ad10679ae5c9c793278f412dc15cd5614af1c53c
Knot <tanawat@senestia.com>
2025-08-01 15:01:05 +0700

    refactor: folder structure

--- app/routes/_components/welcome.tsx
+++ app/routes/_components/welcome.tsx
@@ -1,5 +1,5 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
+import logoDark from "/icons/logo-dark.svg";
+import logoLight from "/icons/logo-light.svg";
 
 export function Welcome() {
   return (
